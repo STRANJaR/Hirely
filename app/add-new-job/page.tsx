1e60acc1493bb7a20fba857480a36229b715c9b0
Rohit Shrivastav <rohitrks805@gmail.com>
2025-04-25 22:17:46 +0530

    API Job listing

--- app/add-new-job/page.tsx
+++ app/add-new-job/page.tsx
@@ -2,35 +2,91 @@ import React from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 
 const AddNewJob = () => {
   return (
-    <section className='w-[60%] mx-auto'>
+    <section className='w-[50%] mx-auto'>
 
-      <div className='py-4'>
+      <div className='py-4 shadow-md p-4 rounded-md mt-4'>
         <h1 className='text-2xl font-bold'>Add New Job</h1>
         <p className='text-xs'>Enter the details of the job you're applying for.</p>
 
-        <div className='flex flex-col gap-4  shadow-md p-4 rounded-md mt-4'>
-          <h2 className='text-lg font-semibold'>Company Information</h2>
+        <form action="">
+          {/* // Company Information  */}
+          <div className='flex flex-col gap-4  '>
+            <h2 className='text-lg font-semibold'>Company Information</h2>
 
-          <form action="">
-            <div className='flex flex-row gap-2 w-full'>
+            <div className='flex flex-col gap-2 w-full'>
+              <div className='flex flex-row w-full gap-8'>
 
-              <div className='w-full'>
-                <Label htmlFor="company" className='text-sm font-semibold'>Company Name</Label>
-                <Input type="text" placeholder='e.g. Google, Microsoft' className='w-full outline-none' />
+                <div className='w-full'>
+                  <Label htmlFor="company" className='text-sm font-semibold'>Company Name</Label>
+                  <Input type="text" placeholder='e.g. Google, Microsoft' className='w-full outline-none' />
+                </div>
+
+                <div className='w-full'>
+                  <Label htmlFor="company-website" className='text-sm font-semibold'>Company Website</Label>
+                  <Input type="text" placeholder='https://example.com' className='w-full' />
+                </div>
               </div>
+              <Label htmlFor="location" className='text-sm font-semibold'>Location</Label>
+            </div>
+            <Input type="text" placeholder='e.g. New Delhi, Gurgaon, Noida or Remote' className='w-full outline-none' />
+            <Separator className='my-4' />
+          </div>
 
-              <div className='w-full'>
-                <Label htmlFor="company" className='text-sm font-semibold'>Company Name</Label>
-                <Input type="text" placeholder='https://example.com' className='w-full' />
+          {/* Job Details  */}
+
+          <div className='flex flex-col gap-4'>
+            <h2 className='text-lg font-semibold'>Job Details</h2>
+
+            <div className='flex flex-col gap-2 w-full'>
+              <div className='flex flex-row w-full gap-8'>
+                <div className='w-full'>
+                  <Label htmlFor="company" className='text-sm font-semibold'>Job Title (Role)</Label>
+                  <Input type="text" placeholder='e.g. Frontend Engineer' className='w-full outline-none' />
+                </div>
+
+                <div className='w-full'>
+                  <Label htmlFor="company-website" className='text-sm font-semibold'>Job Type</Label>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className='w-full '>Full-time</DropdownMenuTrigger>
+                    <DropdownMenuContent className='w-full'>
+                      <DropdownMenuLabel>Part-time</DropdownMenuLabel>
+                      <DropdownMenuLabel>Internship</DropdownMenuLabel>
+                      <DropdownMenuLabel>Freelance</DropdownMenuLabel>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+
+                </div>
+              </div>
+              <div className='flex flex-row w-full gap-8'>
+                <div className='w-full'>
+                  <Label htmlFor="company" className='text-sm font-semibold'>Salary Range (Optional)</Label>
+                  <Input type="text" placeholder='e.g. INR 250000-300000' className='w-full outline-none' />
+                </div>
+
+                <div className='w-full'>
+                  <Label htmlFor="company-website" className='text-sm font-semibold'>Job Posting URL</Label>
+                  <Input type="text" placeholder='https://careers.example.com/job/123' className='w-full' />
+                </div>
               </div>
             </div>
-          </form>
-        </div>
+            <Separator className='my-4' />
+          </div>
 
+        </form>
       </div>
 
     </section>
