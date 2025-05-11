fa1a03d7c31547edbf1e43696e1f7344df9c6106
Rohit Shrivastav <rohitrks805@gmail.com>
2025-05-11 12:40:09 +0530

    bug fix in job api

--- app/api/job/[id]/route.ts
+++ app/api/job/[id]/route.ts
@@ -1,46 +1,45 @@
 import Job from "@/models/Job.model";
 import { NextRequest, NextResponse } from "next/server";
+import { dbConnect } from "@/utils/db";
 
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+    await dbConnect();
+    const { id } = context.params;
+
     try {
-        const existingJob = await Job.findById(id)
-        if (!existingJob) {
-            return new Response("Job not found", { status: 404 });
-        }
-        return NextResponse.json(existingJob, { status: 200 });
-    } catch (error) {
-        return new Response("Failed to find job", { status: 500 });
-
+        const job = await Job.findById(id);
+        if (!job) return new Response("Job not found", { status: 404 });
+        return NextResponse.json(job, { status: 200 });
+    } catch (err) {
+        return new Response("Failed to fetch job", { status: 500 });
     }
 }
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
-    try {
-        const data = await request.json();
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+    await dbConnect();
+    const { id } = context.params;
 
-        const existingJob = await Job.findById(id);
-        if (!existingJob) {
-            return NextResponse.json({ message: "Job not found" }, { status: 404 });
-        }
-        const updatedJobApplication = await Job.findOneAndUpdate({ _id: id }, data, { new: true })
-        return NextResponse.json({ message: "Job updated successfully", updatedJobApplication }, { status: 200 });
-    } catch (error) {
+    try {
+        const data = await req.json();
+        const updatedJob = await Job.findByIdAndUpdate(id, data, { new: true });
+
+        if (!updatedJob) return NextResponse.json({ message: "Job not found" }, { status: 404 });
+
+        return NextResponse.json({ message: "Job updated", updatedJob }, { status: 200 });
+    } catch (err) {
         return NextResponse.json({ message: "Failed to update job" }, { status: 500 });
-
     }
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+    await dbConnect();
+    const { id } = context.params;
 
     try {
         await Job.findByIdAndDelete(id);
-        return NextResponse.json({ message: "Job deleted successfully" }, { status: 200 });
-    } catch (error) {
+        return NextResponse.json({ message: "Job deleted" }, { status: 200 });
+    } catch (err) {
         return NextResponse.json({ message: "Failed to delete job" }, { status: 500 });
-
     }
 }
